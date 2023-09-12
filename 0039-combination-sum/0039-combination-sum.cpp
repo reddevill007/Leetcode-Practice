150aeebb1ab0ce08816d6f3472da5221d48972a1
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-09-12 16:19:14 +0530

    Time: 44 ms (21.93%), Space: 48.1 MB (14.5%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    void solve(vector<int> candidates, int target, int ind, vector<vector<int>> &ans, vector<int> &ds) {
+        if(ind == candidates.size()) {
+            if(target == 0) {
+                ans.push_back(ds);
+            }
+            return;
+        }
+        
+        if(candidates[ind] <= target) {
+            ds.push_back(candidates[ind]);
+            solve(candidates, target - candidates[ind], ind, ans, ds);
+            ds.pop_back();
+        }
+
+        solve(candidates, target, ind + 1, ans, ds);
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        int ind = 0;
+
+        solve(candidates, target, ind, ans, ds);
+
+        return ans;
+    }
+};
