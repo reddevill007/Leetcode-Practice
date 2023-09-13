6d2fc0ed637df6b08e070297250ad0c91c7f4b9c
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-09-13 22:25:14 +0530

    Time: 13 ms (86.34%), Space: 18.2 MB (24.67%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -12,6 +12,7 @@ public:
                 candies[i] = candies[i - 1] + 1;
             }
         }
+
         
         for(int i = n - 2; i >= 0; i--) {
             if(ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
@@ -19,12 +20,12 @@ public:
             }
         }
 
-        int totalCandies = 0;
+        int ct = 0;
 
-        for(int i = 0; i < n; i++) {
-            totalCandies += candies[i];
+        for(auto it: candies) {
+            ct += it;
         }
 
-        return totalCandies;
+        return ct;
     }
 };
