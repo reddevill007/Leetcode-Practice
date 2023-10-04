b344c56ab1e5425e941021b9c67d686d35b3d18d
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-10-04 07:37:31 +0530

    Time: 3 ms (42.79%), Space: 7.6 MB (7.35%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -3,11 +3,11 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int numberOfGoodPairs = 0;
         int n = nums.size();
+        unordered_map<int, int> count;
 
         for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(nums[i] == nums[j]) numberOfGoodPairs++;
-            }
+            numberOfGoodPairs += count[nums[i]];
+            count[nums[i]]++;
         }
 
         return numberOfGoodPairs;
