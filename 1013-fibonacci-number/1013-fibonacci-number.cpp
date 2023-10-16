4616c1d7d0244ba0620b518cb2592b847d02b263
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-10-16 10:25:55 +0530

    Time: 3 ms (57.08%), Space: 6.5 MB (11.69%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    int dp[31];
-
-    int solve(int n) {
-        if(n <= 1) return n;
-
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = solve(n - 1) + solve(n - 2);
-    }
 public:
     int fib(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        if(n <= 1) return n;
+        vector<int> dp(n + 1, -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 };
