24a0d5c7da3cebf1c388166c91e5e8eb54a2338d
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-10-16 10:29:27 +0530

    Time: 0 ms (100%), Space: 6.3 MB (21.89%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int fib(int n) {
         if(n <= 1) return n;
-        vector<int> dp(n + 1, -1);
 
-        dp[0] = 0;
-        dp[1] = 1;
+        int first = 0;
+        int second = 1;
 
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int third = first + second;
+            first = second;
+            second = third;
         }
 
-        return dp[n];
+        return second;
     }
 };
