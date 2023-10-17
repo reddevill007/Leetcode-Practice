8f90094966413d785473995b8a7d9f166c95bc50
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-10-17 08:34:28 +0530

    Time: 3 ms (36.77%), Space: 8.1 MB (52.3%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    int dp[101];
+
+    int solve(vector<int>& nums, int idx, int n) {
+        if(idx > n) {
+            return 0;
+        }
+
+        if(dp[idx] != -1) return dp[idx];
+
+        int steal = nums[idx] + solve(nums, idx + 2, n);
+        int skip = solve(nums, idx + 1, n);
+
+        return dp[idx] = max(skip, steal);
+    }
+
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+
+        memset(dp, -1, sizeof(dp));
+        int take_0th_index = solve(nums, 0, n - 2);
+
+        memset(dp, -1, sizeof(dp));
+        int take_1th_index = solve(nums, 1, n - 1);
+
+        return max(take_0th_index, take_1th_index);
+    }
+};
