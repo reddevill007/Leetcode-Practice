2643b1a484132f62c6ccb048e1fa05f4b6a5cad7
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-10-22 09:17:44 +0530

    Time: 218 ms (26.27%), Space: 23.3 MB (32.1%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,29 +1,47 @@
 class Solution {
-    int n;
-    int dp[1001][1001];
-    int solve(vector<vector<int>>& pairs, int prev, int curr) {
-        if(curr >= n) return 0;
+    // int n;
+    // int dp[1001][1001];
+    // int solve(vector<vector<int>>& pairs, int prev, int curr) {
+    //     if(curr >= n) return 0;
 
-        if(prev != -1 && dp[curr][prev] != -1) return dp[curr][prev];
+    //     if(prev != -1 && dp[curr][prev] != -1) return dp[curr][prev];
 
-        int take = 0;
-        if(prev == -1 || pairs[curr][0] > pairs[prev][1]) {
-            take = 1 + solve(pairs, curr, curr + 1);
-        }
-        int skip = solve(pairs, prev, curr + 1);
+    //     int take = 0;
+    //     if(prev == -1 || pairs[curr][0] > pairs[prev][1]) {
+    //         take = 1 + solve(pairs, curr, curr + 1);
+    //     }
+    //     int skip = solve(pairs, prev, curr + 1);
 
-        if(prev != -1) {
-            dp[curr][prev] = max(take, skip);
-        }
+    //     if(prev != -1) {
+    //         dp[curr][prev] = max(take, skip);
+    //     }
 
-        return max(take, skip);
-    }
+    //     return max(take, skip);
+    // }
 
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        memset(dp, -1, sizeof(dp));
-        n = pairs.size();
+        // memset(dp, -1, sizeof(dp));
+        // n = pairs.size();
+        // sort(begin(pairs), end(pairs));
+        // return solve(pairs, -1, 0);
+
+        
+        int n = pairs.size();
+        vector<int> dp(n, 1);
         sort(begin(pairs), end(pairs));
-        return solve(pairs, -1, 0);
+
+        int maxLts = 1;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(pairs[i][0] > pairs[j][1]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    maxLts = max(maxLts, dp[i]);
+                }
+            }
+        }
+
+        return maxLts;
     }
 };
