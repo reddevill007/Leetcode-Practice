3b6fb621e194c8654e74f0e597483b973ef11785
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-10-24 00:46:06 +0530

    Time: 134 ms (26.84%), Space: 7.5 MB (64.59%) - LeetHub

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,30 +1,33 @@
 class Solution {
-public:
-    int MOD = 1e9 + 7;
-    // dp[idx][searchCost][maxSoFar]
     int dp[51][51][101];
+    int MOD = 1e9 + 7;
 
-    int solve(int idx, int searchCost, int maxSoFar, int length, int range, int expectedCost) {
-        if(idx == length) {
-            if(searchCost == expectedCost) return 1;
-            return 0;
+    int solve(int idx, int search_cost, int max_so_far, int len, int range, int expected_cost) {
+        if(idx == len) {
+            if(search_cost == expected_cost) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
         }
 
-        if(dp[idx][searchCost][maxSoFar] != -1) return dp[idx][searchCost][maxSoFar];
+        if(dp[idx][search_cost][max_so_far] != -1) return dp[idx][search_cost][max_so_far];
 
         int result = 0;
 
         for(int i = 1; i <= range; i++) {
-            if(i > maxSoFar) {
-                result = (result + solve(idx + 1, searchCost + 1, i, length, range, expectedCost)) % MOD;
+            if(i > max_so_far) {
+                result = (result + solve(idx + 1, search_cost + 1, i, len, range, expected_cost)) % MOD;
             } else {
-                result = (result + solve(idx + 1, searchCost, maxSoFar, length, range, expectedCost)) % MOD;
+                result = (result + solve(idx + 1, search_cost, max_so_far, len, range, expected_cost)) % MOD;
             }
         }
 
-        return dp[idx][searchCost][maxSoFar] = result % MOD;
+        return dp[idx][search_cost][max_so_far] = result;
     }
 
+public:
     int numOfArrays(int n, int m, int k) {
         memset(dp, -1, sizeof(dp));
         return solve(0, 0, 0, n, m, k);
