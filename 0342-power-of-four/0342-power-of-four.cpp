add5e79e1d6fc1cb1a63c309139aa9539cc9e40e
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-10-23 10:28:47 +0530

    Time: 0 ms (100%), Space: 6.2 MB (47.7%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 0) {
-            return false;
-        }
+        // if(n == 0 || n == 1) {
+        //     return n;
+        // }
 
-        while(n % 4 == 0) {
-            n = n / 4;
-        }
+        // while(n % 4 == 0) {
+        //     n = n / 4;
+        // }
 
-        if(n == 1) return true;
-        return false;
+        // if(n == 1) return true;
+        // return false;
+
+        // int x = (log(n) / log(4));
+
+        // if(pow(4, x) == n) return true;
+        // return false;
+
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
     }
 };
