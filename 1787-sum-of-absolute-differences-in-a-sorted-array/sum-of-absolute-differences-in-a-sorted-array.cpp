9569d7b41fa054ff9c040af94f5aeebef92b3b28
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-11-26 13:59:50 +0530

    Time: 95 ms (47.31%) | Memory: 86 MB (43.55%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
-        int sum = 0;
-        for (int n : nums) {
-            sum += n;
+        /*
+            nums -> sorted in non decending order
+            res -> same len (res[i] = sum(|nums[i]-nums[j]|))
+
+            eg -> nums = [2, 3, 5] => 10
+                  ans = [4, 3, 5]
+                  prefix = [2, 5, 10]
+                  suffix = [10, 8, 5]
+
+            eg -> nums = [1, 4, 6, 8, 10]
+                  ans  = [24, 15, 13, 15, 21]
+                  prefix = [1, 5, 11, 19, 29]
+                  suffix = [29, 28, 24, 18, 10]
+
+            formula: res[i] = nums[i]*i - leftSum + rightSum - nums[i]*(n - i - 1)
+
+        */
+        int n = nums.size();
+        vector<int> res(n);
+        vector<int> prefix(n);
+
+        prefix[0] = nums[0];
+
+        for(int i = 1; i < n; i++) {
+            prefix[i] = nums[i] + prefix[i - 1];
         }
-        int left = 0;
-        int right = sum;
 
-        std::vector<int> result(nums.size(), 0);
+        for(int i = 0; i < n; i++) {
+            int leftSum = prefix[i] - nums[i];
+            int rightSum = prefix[n - 1] - prefix[i];
 
-        for (int i = 0; i < nums.size(); i++) {
-            int n = nums[i];
-            right -= n;
-
-            result[i] = n * i - left + right - n * (nums.size() - i - 1);
-
-            left += n;
+            res[i] = nums[i]*i - leftSum + rightSum - nums[i]*(n - i - 1);
         }
 
-        return result;
+        return res;
     }
 };
