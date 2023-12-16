7880b4c0f013215bc968df6deaa913bc666c570a
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-12-16 13:49:03 +0530

    Time: 33 ms (6.24%) | Memory: 20.8 MB (25.53%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        set<string> seen;
+
+        for (int i=0; i<9; ++i) {
+            for (int j=0; j<9; ++j) {
+                char number = board[i][j];
+
+                if(number != '.') {
+                    if(!seen.insert(to_string(number) + " in row " + to_string(i)).second || !seen.insert(to_string(number) + " in col " + to_string(j)).second || !seen.insert(to_string(number) + " in box " + to_string(i / 3) + " and " + to_string(j / 3)).second) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+
+    }
+};
