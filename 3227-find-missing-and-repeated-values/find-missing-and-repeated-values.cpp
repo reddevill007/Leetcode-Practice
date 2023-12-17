73a7b28bc8983b3a5a5362c7d8e820704609738a
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-12-17 11:08:23 +0530

    Time: 46 ms (50.00%) | Memory: 28.1 MB (62.50%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int> newArr;
         int n = grid.size();
-        
+        int limit = n*n;
+
+        unordered_set<int> el_count;
+        vector<int> ans(2, -1);
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                newArr.push_back(grid[i][j]);
+                if(!el_count.insert(grid[i][j]).second) {
+                    ans[0] = grid[i][j];
+                }
+                el_count.insert(grid[i][j]);
             }
-        }
-        
-        sort(newArr.begin(), newArr.end());
-        vector<int> arr(2, -1);
-        
-        for(int i = 0; i < n * n - 1; i++) {
-            if (newArr[i] == newArr[i + 1]) {
-                arr[0] = newArr[i]; // Repeating number
-            }
-            else if (newArr[i] + 1 != newArr[i + 1]) {
-                arr[1] = newArr[i] + 1; // Missing number
-            }
-        }
-        
-        if(newArr[0] != 1) arr[1] = 1;
-
-        // Handle the case when the missing number is at the end
-        if (arr[1] == -1 && newArr[n * n - 1] != n * n) {
-            arr[1] = n * n; // Missing number
         }
 
-        return arr;
+        for(int i = 1; i <= limit; i++) {
+            if(!el_count.insert(i).second) {
+                continue;
+            } else {
+                ans[1] = i;
+            }
+        }
+
+        return ans;
     }
 };
