4209ce37e5736de83681a21c8720bb6a0547c034
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-12-24 10:26:28 +0530

    Time: 9 ms (8.31%) | Memory: 8.5 MB (5.93%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    string createAlterntiveString(int n, char startWith) {
+        string formed = "";
+        formed.push_back(startWith);
+
+        for(int i = 1; i < n; i++) {
+            char prevChar = formed[i - 1];
+            char toBePushed  = prevChar == '1' ? '0' : '1';
+            formed.push_back(toBePushed);
+        }
+
+        return formed;
+    }
+
+public:
+    int minOperations(string s) {
+        int n = s.length();
+
+        string finalStringStaringWith1 = createAlterntiveString(n, '1');
+        string finalStringStaringWith0 = createAlterntiveString(n, '0');
+
+        cout << "End str1 " << finalStringStaringWith1 << endl;
+        cout << "End str0 " << finalStringStaringWith0 << endl;
+
+        int diff1 = 0;
+        int diff0 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] != finalStringStaringWith1[i]) diff1++;
+            if(s[i] != finalStringStaringWith0[i]) diff0++;
+        }
+
+        return min(diff1, diff0);
+    }
+};
