49a615ae744aa0d64240a0961e28305e980bf6fa
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2023-12-25 10:40:27 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (65.44%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,8 +2,25 @@ class Solution {
     int dp[101];
 public:
     int numDecodings(string s) {
-        memset(dp, -1, sizeof(dp));
-        return recursiveDecode(s, 0);
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+
+        dp[n] = 1;
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if(i + 1 < n) {
+                    if(s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')) {
+                        dp[i] += dp[i + 2];
+                    }
+                }
+            }
+        }
+
+        return dp[0];
     }
 
     int recursiveDecode(string s, int idx) {
