4cec14c4b95cd64230fb8fde8fd441d66043e063
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2024-10-04 00:40:40 +0530

    Time: 100 ms (45.59%), Space: 61.5 MB (28.4%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,9 +1,26 @@
 class Solution {
+    // void print_priority_queue(priority_queue<int, vector<int>, greater<int>> pq) {
+    //     cout << "[ ";
+    //     while (!pq.empty()) {
+    //         cout << pq.top() << " ";
+    //         pq.pop();
+    //     }
+    //     cout << "]" << endl;
+    // }
+
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
-        for(auto it: nums) pq.push(it);
-        for(int i = 0; i < k - 1; i++) pq.pop();
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto it: nums) {
+            pq.push(it);
+            // print_priority_queue(pq);
+            if(pq.size() > k) {
+                pq.pop();
+                // print_priority_queue(pq);
+            }
+        }
+        // for(int i = 0; i < k - 1; i++) pq.pop();
+        cout << "returning " << pq.top() << endl;
         return pq.top();
     }
 };
