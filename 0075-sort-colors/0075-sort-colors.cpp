9ef481c564352694b25028dc724c566962386bee
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2024-10-05 15:43:48 +0530

    Time: 0 ms (100%), Space: 10.4 MB (34.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero = 0;
-        int one = 0;
-        int two = 0;
+        vector<int> count(3, 0);
 
         for(auto it: nums) {
-            if(it == 0) zero++;
-            if(it == 1) one++;
-            if(it == 2) two++;
+            count[it]++;
         }
 
         int idx = 0;
 
-        while(zero--) {
-            nums[idx] = 0;
-            idx++;
-        }
-
-        while(one--) {
-            nums[idx] = 1;
-            idx++;
-        }
-
-        while(two--) {
-            nums[idx] = 2;
-            idx++;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < count[i]; j++) {
+                nums[idx] = i;
+                idx++;
+            }
         }
     }
 };
