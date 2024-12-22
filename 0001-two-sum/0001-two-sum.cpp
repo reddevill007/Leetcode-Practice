a0a53c65e3c1198076e9d59bf97db17bcf375ed7
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2024-12-22 22:05:05 +0530

    Time: 8 ms (49.66%), Space: 16.1 MB (5.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,16 +4,14 @@ public:
         unordered_map<int, int> nums_idx_mp;
 
         for(int i = 0; i < nums.size(); i++) {
-            cout << "Pushing " << nums[i] << " " << i << endl;
-            nums_idx_mp.insert({nums[i], i});
+            nums_idx_mp[nums[i]] = i;
         }
 
         for(int i = 0; i < nums.size(); i++) {
             int comp = target - nums[i];
 
-            if(nums_idx_mp.find(comp) != nums_idx_mp.end() && nums_idx_mp.find(comp)->second != i) {
-                cout << "found " << nums_idx_mp.find(comp)->second << " " << i << endl;
-                return {nums_idx_mp.find(comp)->second, i};
+            if(nums_idx_mp.contains(comp) && nums_idx_mp[comp] != i) {
+                return {nums_idx_mp[comp], i};
             }
         }
 
