8f2dd87f54b8f7ef44ad00dfcee1de14cc15abf5
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2024-12-22 22:00:04 +0530

    Time: 135 ms (8.06%), Space: 16 MB (7.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        unordered_map<int, int> nums_idx_mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            cout << "Pushing " << nums[i] << " " << i << endl;
+            nums_idx_mp.insert({nums[i], i});
+        }
 
         for(int i = 0; i < nums.size(); i++) {
             int comp = target - nums[i];
 
-            if(mp.contains(comp) && mp[comp] != i) {
-                return {i, mp[comp]};
+            if(nums_idx_mp.find(comp) != nums_idx_mp.end() && nums_idx_mp.find(comp)->second != i) {
+                cout << "found " << nums_idx_mp.find(comp)->second << " " << i << endl;
+                return {nums_idx_mp.find(comp)->second, i};
             }
-
-            mp[nums[i]] = i;
         }
 
         return {};
