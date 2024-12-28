a55e832f18d0cb07433e17e7e2cf356c97ac7a69
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2024-12-28 17:01:35 +0530

    Time: 2 ms (32.48%), Space: 45.8 MB (33.76%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,19 +1,17 @@
 class Solution {
 
 public:
-    int maxScoreSightseeingPair(vector<int>& values) {
-        int ans = values[0];
-        int n = values.size();
-        vector<int> iMaxSoFar(n, 0);
+    int maxScoreSightseeingPair(vector<int>& v) {
+        int ans = v[0];
+        int n = v.size();
 
-        iMaxSoFar[0] = values[0];
+        int maxLeftScore = v[0];
+
         for(int i = 1; i < n; i++) {
-            iMaxSoFar[i] = max(values[i] + i, iMaxSoFar[i - 1]);
-        }
-
-        for(int j = n - 1; j > 0; j--) {
-            int score = iMaxSoFar[j - 1] + values[j] - j;
-            ans = max(ans, score);
+            int currRightScore = v[i] - i;
+            ans = max(ans, currRightScore + maxLeftScore);
+            int currLeftScore = v[i] + i;
+            maxLeftScore = max(maxLeftScore, currLeftScore);
         }
 
         return ans;
