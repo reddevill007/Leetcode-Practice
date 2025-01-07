b7ebd3b2ef71e51166a5f9438a57957efa5733a0
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2025-01-07 23:13:28 +0530

    Time: 10 ms (19.94%), Space: 26 MB (17.79%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,21 +3,19 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         set<string> seen;
 
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
-                char number = board[i][j];
-
-                if(number != '.') {
-                    if(!seen.insert(to_string(number) + " in row " + to_string(i)).second ||
-                        !seen.insert(to_string(number) + " in column " + to_string(j)).second ||
-                        !seen.insert(to_string(number) + " in block " + to_string(i/3) + "-" + to_string(j/3)).second) {
-                            return false;
-                        }
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.') continue;
+                else {
+                    if(
+                        !seen.insert(to_string(board[i][j]) + "_in_row_" + to_string(i)).second
+                        || !seen.insert(to_string(board[i][j]) + "_in_col_" + to_string(j)).second
+                        || !seen.insert(to_string(board[i][j]) + "_in_" + to_string(i / 3) + "_and_" + to_string(j / 3)).second
+                    ) return false;
                 }
             }
         }
 
         return true;
-
     }
 };
