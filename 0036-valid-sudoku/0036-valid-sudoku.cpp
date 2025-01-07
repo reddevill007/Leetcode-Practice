315a13b602324a41c36de247ddec57999a2093c2
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2025-01-07 23:16:38 +0530

    Time: 20 ms (5.48%), Space: 25.9 MB (20.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -5,12 +5,11 @@ public:
 
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
-                if(board[i][j] == '.') continue;
-                else {
+                if(board[i][j] != '.') {
                     if(
-                        !seen.insert(to_string(board[i][j]) + "_in_row_" + to_string(i)).second
-                        || !seen.insert(to_string(board[i][j]) + "_in_col_" + to_string(j)).second
-                        || !seen.insert(to_string(board[i][j]) + "_in_" + to_string(i / 3) + "_and_" + to_string(j / 3)).second
+                        !seen.insert(to_string(board[i][j]) + "x" + to_string(i)).second
+                        || !seen.insert(to_string(board[i][j]) + "y" + to_string(j)).second
+                        || !seen.insert(to_string(board[i][j])+ to_string(i / 3) + "xy" + to_string(j / 3)).second
                     ) return false;
                 }
             }
