a2f9803ba0f1a7476bcb48039033923f426f6393
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2025-01-10 22:17:31 +0530

    Time: 0 ms (100%), Space: 9.8 MB (59.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,13 +1,27 @@
 class Solution {
+private:
+    bool isalnum_custom(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return true;
+        }
+        if (ch >= 'A' && ch <= 'Z') {
+            return true;
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            return true;
+        }
+        return false;
+    }
+
 public:
     bool isPalindrome(string s) {
         int i = 0;
         int j = s.length() -1;
 
         while(i < j) {
-            if(!isalnum(s[i])) {
+            if(!isalnum_custom(s[i])) {
                 i++;
-            } else if(!isalnum(s[j])) {
+            } else if(!isalnum_custom(s[j])) {
                 j--;
             } else {
                 if(tolower(s[i]) != tolower(s[j])) return false;
